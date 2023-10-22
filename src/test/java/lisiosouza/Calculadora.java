57e5cd76290c8cc2560657a245cdf33e605ca989
LisioSouza <lisioinsystem@gmail.com>
2023-10-22 12:18:39 -0300

    Implementaçaãode primeiro teste - calculadora de soma

---
+++ src/test/java/lisiosouza/Calculadora.java
@@ -0,0 +1,10 @@
+package lisiosouza;
+
+public class Calculadora {
+    public int somar(int x, int y) {
+        return x + y;
+    }
+}
+
+
+
---
+++ src/test/java/lisiosouza/Calculadorateste.java
@@ -0,0 +1,16 @@
+package lisiosouza;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Calculadorateste {
+    @Test
+
+    public void testSomar() {
+        Calculadora calculadora = new Calculadora();
+
+        int resultado = calculadora.somar(2, 3);
+        Assertions.assertEquals(5, resultado);
+
+    }
+}
